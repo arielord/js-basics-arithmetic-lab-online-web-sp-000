644646e5b08728d81004d0d1b5f4530cad292fe2
Ariel <arielord@buffalo.edu>
2019-08-12 18:42:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random(20);
 let randomInteger = Math.floor(randomNumber)
-let randomUserID = Math.round(Math.random(1000000001, 1000000021));
+let randomUserID = Math.round(Math.random(1000000001, 1000000020));
